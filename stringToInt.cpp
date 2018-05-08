40618c5761b85a5f6ffb7e20c1398811d1e114be
Ruturaj Mohanty <kanha95@gmail.com>
2018-05-08 20:34:50 +0530

    String to int

---
+++ stringToInt.cpp
@@ -0,0 +1,23 @@
+
+#include<bits/stdc++.h>
+
+using namespace std;
+
+
+
+int main(){
+
+
+string s="44";
+
+char p[s.length()+1];
+
+strcpy(p,s.c_str());
+
+int x=atoi(p);
+
+cout<<x;
+
+
+return 0;
+}
